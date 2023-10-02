4aa7f1f69ffcac03bc2efaf1e17bdb13d639b752
Aleksei Kudinov <reanim86@yandex.ru>
2023-10-02 08:14:55 +0300

    fix

--- telegram.py
+++ telegram.py
@@ -1,7 +1,7 @@
 import requests
 
 def send_mes():
-    TOKEN = "6569945873:AAGsZNz5pPLLeAJiHySUfJ5e3828K0jTAE4"
+    TOKEN = ""
     chat_id = "64619556"
     message = "bot message"
     url = f"https://api.telegram.org/bot{TOKEN}/sendDocument?chat_id={chat_id}', document='./stats/2023-09-28.csv')"
