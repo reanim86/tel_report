38c7423bfc28732df488b20ee8881e52860813e7
Aleksei Kudinov <reanim86@yandex.ru>
2023-10-26 09:10:30 +0300

    add open file

--- telegram.py
+++ telegram.py
@@ -1,10 +1,15 @@
 import requests
+import configparser
 
 def send_mes():
-    TOKEN = ""
+    config = configparser.ConfigParser()
+    config.read('settings.ini')
+    token = config['Tg']['token']
     chat_id = "64619556"
-    message = "bot message"
-    url = f"https://api.telegram.org/bot{TOKEN}/sendDocument?chat_id={chat_id}', document='./stats/2023-09-28.csv')"
+    # message = "bot message"
+    with open("./stats/2023-09-28.csv", "rb") as filexlsx:
+        files = {"document": filexlsx}
+    url = f"https://api.telegram.org/bot{token}/sendDocument?chat_id={chat_id}', files=files)"
     print(requests.get(url).json())  # Эта строка отсылает сообщение
     return
 send_mes()
